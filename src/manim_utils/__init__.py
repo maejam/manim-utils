19e5be963c390c03565fbfab513d7ce109ec6afa
maejam <git.rejoice553@passmail.net>
2026-04-28 22:52:55 +0200

    fix(mobjects): add import for IconText in __init__.py

--- src/manim_utils/__init__.py
+++ src/manim_utils/__init__.py
@@ -2,4 +2,5 @@ from .animations import LazyAnimation as LazyAnimation
 from .animations import TrackedAnimationMixin as TrackedAnimationMixin
 from .code import get_styles_list as get_styles_list
 from .code import highlight_code as highlight_code
+from .mobjects import IconText as IconText
 from .stencil import Stencil as Stencil
