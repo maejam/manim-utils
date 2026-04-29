9c9f92f2582194e52e499db387d9daa0344bd1fd
maejam <git.rejoice553@passmail.net>
2026-04-29 20:40:13 +0200

    fix(groups): turn (V)IconText into (V)Groups
    Because they don't have their own points and only act as containers, it
    makes more sense semantically to treat them as Groups.

--- src/manim_utils/__init__.py
+++ src/manim_utils/__init__.py
@@ -3,6 +3,6 @@ from .animations import TrackedAnimationMixin as TrackedAnimationMixin
 from .code import get_styles_list as get_styles_list
 from .code import highlight_code as highlight_code
 from .groups import GroupDict as GroupDict
-from .mobjects import IconText as IconText
-from .mobjects import VIconText as VIconText
+from .groups import IconText as IconText
+from .groups import VIconText as VIconText
 from .stencil import Stencil as Stencil
