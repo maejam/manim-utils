d1050db9381ba15c857c93ab8870b27b00161f63
maejam <git.rejoice553@passmail.net>
2026-04-29 03:41:16 +0200

    fix(mobjects): import VIconText in __init__.py

--- src/manim_utils/__init__.py
+++ src/manim_utils/__init__.py
@@ -4,4 +4,5 @@ from .code import get_styles_list as get_styles_list
 from .code import highlight_code as highlight_code
 from .groups import GroupDict as GroupDict
 from .mobjects import IconText as IconText
+from .mobjects import VIconText as VIconText
 from .stencil import Stencil as Stencil
